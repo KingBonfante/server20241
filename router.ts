f4d294ba198ec552e8636cdfead41778ee682e72
KingBonfante <72134861+KingBonfante@users.noreply.github.com>
2024-06-05 00:46:38 +0000

    feat: cadastro e listagem de estudante

--- router.ts
+++ router.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { listCourse, saveCourse } from "./controllers/course";
+import { listStudent, saveStudent } from "./controllers/student";
 const router = Router();
 
 router.get("/courses", listCourse);
-router.post("/courses", saveCourse)
+router.post("/courses", saveCourse);
+router.get("/students", listStudent);
+router.post("/students", saveStudent)
 
 export { router };
