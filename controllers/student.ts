43bc0e35bf9b1f545cde3b27f572a098815c2d68
KingBonfante <72134861+KingBonfante@users.noreply.github.com>
2024-06-11 23:14:11 +0000

    fix: ajuste de conexão banco de dados

--- controllers/student.ts
+++ controllers/student.ts
@@ -2,19 +2,33 @@ import { pool } from "../shared/database";
 import { Request, Response, Router } from "express";
 
 export async function listStudent(req: Request, res: Response) {
-  //conecta com o banco
+  
   const client = await pool.connect();
-  //realiza consulta sql
-  const students = await client.query(`select * from students`)
-  //retorna consulta em formato json
-  return res.status(200).json(students.rows);
+  try {
+    const students = await client.query(`select * from students`)
+    if (students.rowCount === 0) {
+      return res.status(404).json({message: "não encontrado"})
+    } 
+    return res.status(200).json(students.rows);
+  } catch (error) {
+    console.log(error)
+  }finally{
+    client.release()
+  }
 }
+
+
 export async function saveStudent(req: Request, res: Response) {
+  const client = await pool.connect();
   const student = req.body;
   console.log(student)
-  //conecta com o banco
-  const client = await pool.connect();
-  //realiza consulta sql
-  const response = await client.query(`INSERT INTO students (name, email) VALUES ('${student.name}','${student.email}')`)
-  res.status(201).json(response);
+  try {
+    const response = await client.query(`INSERT INTO students (name, email) VALUES ('${student.name}','${student.email}')`);
+    console.log(response.rows[0]);
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(400).json({message: 'Dados inválidos', error});
+  }finally{
+    client.release();
+  }  
 }
