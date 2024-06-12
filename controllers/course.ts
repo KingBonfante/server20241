feb64cc7d57a87425ae1440a88e0171b99856b41
KingBonfante <72134861+KingBonfante@users.noreply.github.com>
2024-06-12 00:14:00 +0000

    feat: delete course

--- controllers/course.ts
+++ controllers/course.ts
@@ -24,3 +24,16 @@ export async function saveCourse(req: Request, res: Response) {
     client.release()
   }
 }
+
+export async function deleteCourse(req: Request, res: Response) {
+  const client = await pool.connect();
+  const id = req.params.id;
+  try {
+    const response = await client.query(`DELETE FROM courses WHERE id=${id}`);
+    res.status(200).json({message: "Registro Excluído"});
+  } catch (error) {
+    res.status(404).json({ message: error });
+  } finally {
+    client.release();
+  }
+}
--- router.ts
+++ router.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
-import { listCourse, saveCourse } from "./controllers/course";
+import { deleteCourse, listCourse, saveCourse } from "./controllers/course";
 import { deleteStudent, listStudent, saveStudent } from "./controllers/student";
 const router = Router();
 
 router.get("/courses", listCourse);
 router.post("/courses", saveCourse);
+router.delete("/courses/:id", deleteCourse)
+
 router.get("/students", listStudent);
 router.post("/students", saveStudent)
-
 router.delete("/students/:id", deleteStudent)
+
 
 export { router };
