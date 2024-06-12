eed6838c22f63e3b2effc09713633fc33d272006
KingBonfante <72134861+KingBonfante@users.noreply.github.com>
2024-06-12 00:03:29 +0000

    fix: função de delete students

--- controllers/student.ts
+++ controllers/student.ts
@@ -2,17 +2,17 @@ import { pool } from "../shared/database";
 import { Request, Response, Router } from "express";
 
 export async function listStudent(req: Request, res: Response) {
-  
+
   const client = await pool.connect();
   try {
     const students = await client.query(`select * from students`)
     if (students.rowCount === 0) {
-      return res.status(404).json({message: "não encontrado"})
-    } 
+      return res.status(404).json({ message: "não encontrado" })
+    }
     return res.status(200).json(students.rows);
   } catch (error) {
     console.log(error)
-  }finally{
+  } finally {
     client.release()
   }
 }
@@ -27,8 +27,21 @@ export async function saveStudent(req: Request, res: Response) {
     console.log(response.rows[0]);
     res.status(201).json(response);
   } catch (error) {
-    res.status(400).json({message: 'Dados inválidos', error});
-  }finally{
+    res.status(404).json({ message: 'Dados inválidos', error });
+  } finally {
     client.release();
-  }  
+  }
+}
+
+export async function deleteStudent(req: Request, res: Response) {
+  const client = await pool.connect();
+  const id = req.params.id
+  try {
+    const response = await client.query(`DELETE FROM students WHERE id=${id}`);
+    res.status(200).json({message: "Registro Excluído"});
+  } catch (error) {
+    res.status(404).json({ message: 'Erro na exclusão', error });
+  } finally {
+    client.release();
+  }
 }
--- router.ts
+++ router.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { listCourse, saveCourse } from "./controllers/course";
-import { listStudent, saveStudent } from "./controllers/student";
+import { deleteStudent, listStudent, saveStudent } from "./controllers/student";
 const router = Router();
 
 router.get("/courses", listCourse);
 router.post("/courses", saveCourse);
 router.get("/students", listStudent);
-router.post("/students", saveStudent)
+router.post("/students/:id", saveStudent)
+
+router.delete("/students", deleteStudent)
 
 export { router };
